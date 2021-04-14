c3368517ef4a70ab77d7354525a6377aa3ab8b49
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-14 02:03:30 -0300

    simple state management and lifecycle methods for component

---
+++ pocs/simple-component-state/src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        Simple Component state
+      </header>
+      <main>
+        <Clock></Clock>
+      </main>
+    </div>
+  );
+}
+
+export default App;
