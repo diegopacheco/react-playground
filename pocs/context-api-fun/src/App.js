2320cfd87c8a8783e1287f5520f45845e22a365e
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-16 16:24:28 -0300

    context api

--- pocs/context-api-fun/src/App.js
+++ pocs/context-api-fun/src/App.js
@@ -1,34 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
-import {ThemeContext, themes} from './theme-context';
-import ThemedButton from './themed-button';
-import React from 'react';
-import Page from './Page';
+import * as React from 'react'
 
-function Toolbar(props) {
-  return (
-    <ThemedButton onClick={props.changeTheme}>
-      Change Theme
-    </ThemedButton>
-  );
+export function CountDisplay() {
+  const {count} = React.useContext(CountContext)
+  return <div><p>Count: {count}</p></div>
+};
+
+
+const CountContext = React.createContext()
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'increment': {
+      return {count: state.count + 1}
+    }
+    case 'decrement': {
+      return {count: state.count - 1}
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`)
+    }
+  }
 }
+function CountProvider({children}) {
+  const [state, dispatch] = React.useReducer(countReducer, {count: 0})
+  // NOTE: you *might* need to memoize this value
+  // Learn more in http://kcd.im/optimize-context
+  const value = {state, dispatch}
+  return <CountContext.Provider value={value}>{children}</CountContext.Provider>
+}
+function useCount() {
+  const context = React.useContext(CountContext)
+  if (context === undefined) {
+    throw new Error('useCount must be used within a CountProvider')
+  }
+  return context
+}
+function CountConsumer({children}) {
+  return (
+    <CountContext.Consumer>
+      {context => {
+        if (context === undefined) {
+          throw new Error('CountConsumer must be used within a CountProvider')
+        }
+        return children(context)
+      }}
+    </CountContext.Consumer>
+  )
+}
+export {CountProvider, useCount}
 
 class App extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      theme: themes.dark,
-    };
-
-    this.toggleTheme = () => {
-      this.setState(state => ({
-        theme:
-          state.theme === themes.dark
-            ? themes.light
-            : themes.dark,
-      }));
-    };
-  }
   render(){
     return (
       <div className="App">
@@ -37,18 +59,12 @@ class App extends React.Component{
           Context Api
         </header>
         <main>
-        <ThemeContext.Provider value={this.state.theme}>
-            <Page>
-              <Toolbar changeTheme={this.toggleTheme} />
-              <div>
-                <ThemedButton />
-              </div>
-            </Page>
-         </ThemeContext.Provider>         
+          <CountProvider>
+            <CountDisplay />
+          </CountProvider>        
         </main>
       </div>
     ); 
   }
 }
-
 export default App;
