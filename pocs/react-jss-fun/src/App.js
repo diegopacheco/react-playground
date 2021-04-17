e3aeb68934972c9220f077d798c53ecff83c3778
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-17 12:56:55 -0300

    react-jss

---
+++ pocs/react-jss-fun/src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+import {createUseStyles} from 'react-jss'
+
+// Create your Styles. Remember, since React-JSS uses the default preset,
+// most plugins are available without further configuration needed.
+const useStyles = createUseStyles({
+  myButton: {
+    color: 'green',
+    margin: {
+      // jss-expand gives more readable syntax
+      top: 5, // jss-default-unit makes this 5px
+      right: 0,
+      bottom: 0,
+      left: '1rem'
+    },
+    '& span': {
+      // jss-nested applies this to a child span
+      fontWeight: 'bold' // jss-camel-case turns this into 'font-weight'
+    }
+  },
+  myLabel: {
+    fontStyle: 'italic'
+  }
+})
+
+const Button = ({children}) => {
+  const classes = useStyles()
+  return (
+    <button className={classes.myButton}>
+      <span className={classes.myLabel}>{children}</span>
+    </button>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        JSS
+      </header>
+      <main>
+        <br/>
+        <Button>Submit</Button>
+      </main>
+    </div>
+  );
+}
+
+export default App;
