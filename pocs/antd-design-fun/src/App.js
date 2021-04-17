7162069f98c9221093f349260af74678ceaf8581
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-17 19:00:06 -0300

    antd

---
+++ pocs/antd-design-fun/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button, DatePicker } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Button type="primary">PRESS ME</Button>
+        <DatePicker placeholder="select date" />
+      </main>
+    </div>
+  );
+}
+
+export default App;
