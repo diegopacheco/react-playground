f0b934ac66709427378b439209157dc57a9eb2dd
diegopacheco <diego.pacheco.it@gmail.com>
2022-08-24 00:12:30 -0700

    React Benchmark working with react 18

---
+++ pocs/react-benchmark-fun/bench-fun/benchmark.js
@@ -0,0 +1,6 @@
+import Component from './src/Component'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function () {
+  return <Component name="Bench!" />
+}
---
+++ pocs/react-benchmark-fun/bench-fun/src/Component.js
@@ -0,0 +1,8 @@
+function Component(props) {
+  return (
+    <div className="Main">
+      Hello World! {props.name}
+    </div>
+  );
+}
+export default Component;
