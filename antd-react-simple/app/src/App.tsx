4b982d7c18152a2caaef0133dd68e9af268c0175
diegopacheco <diego.pacheco.it@gmail.com>
2024-07-19 00:30:04 -0700

    antd

---
+++ antd-react-simple/app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { DatePicker } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DatePicker />
+      </header>
+    </div>
+  );
+}
+
+export default App;
