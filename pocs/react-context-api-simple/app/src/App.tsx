465d9431592068bf6f6a0b2e8ebc609dca28f3fe
diegopacheco <diego.pacheco.it@gmail.com>
2024-07-22 03:14:57 -0700

    context api

--- pocs/react-context-api-simple/app/src/App.tsx
+++ pocs/react-context-api-simple/app/src/App.tsx
@@ -38,6 +38,7 @@ function App() {
           <div>
             <h1>Using React Context API</h1>
             <MyConsumer />
+            <MyConsumer />
           </div>
         </MyProvider>
       </header>
