0e18ae02f823e96f907951d77578ccc057cdaf2d
diegopacheco <diego.pacheco.it@gmail.com>
2024-12-04 00:55:20 -0800

    react-null performance

--- react-null/react-null-app/README.md
+++ react-null/react-null-app/README.md
@@ -1,3 +1,7 @@
+# React Render Null Performance
+
+<img src="react-null.png" />
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- react-null/react-null-app/src/App.js
+++ react-null/react-null-app/src/App.js
@@ -1,6 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { Profiler } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const onRenderCallback = (
+  id, // the "id" prop of the Profiler tree that has just committed
+  phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
+  actualDuration, // time spent rendering the committed update
+  baseDuration, // estimated time to render the entire subtree without memoization
+  startTime, // when React began rendering this update
+  commitTime, // when React committed this update
+  interactions // the Set of interactions belonging to this update
+) => {
+  console.log(`Profiler ID: ${id}`);
+  console.log(`Phase: ${phase}`);
+  console.log(`Actual Duration: ${actualDuration.toFixed(2)} ms`);
+  console.log(`Base Duration: ${baseDuration.toFixed(2)} ms`);
+  console.log(`Start Time: ${startTime.toFixed(2)} ms`);
+  console.log(`Commit Time: ${commitTime.toFixed(2)} ms`);
+  console.log('Interactions:', interactions);
+};
 
 function App() {
   return (
@@ -11,7 +30,9 @@ function App() {
           return null
         </p>
       </header>
-      <Parent />
+      <Profiler id="Parent" onRender={onRenderCallback}>
+        <Parent />
+      </Profiler>
     </div>
   );
 }
