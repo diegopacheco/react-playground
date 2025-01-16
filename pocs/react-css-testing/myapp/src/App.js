252a114b4a3da9858ee3ffdb436e572aa3d46b9a
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-16 00:50:55 -0800

    add tailwind

--- pocs/react-css-testing/myapp/src/App.js
+++ pocs/react-css-testing/myapp/src/App.js
@@ -17,6 +17,9 @@ function App() {
         >
           Learn React
         </a>
+        <h1 className="text-3xl font-bold underline">
+          Hello world from tailwind!
+        </h1>
       </header>
     </div>
   );
