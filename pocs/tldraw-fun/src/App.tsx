29cf8e3d79a7212afebab97c49a944b9cdf50a73
diegopacheco <diego.pacheco.it@gmail.com>
2026-02-04 21:50:36 -0800

    tldraw

---
+++ pocs/tldraw-fun/src/App.tsx
@@ -0,0 +1,10 @@
+import { Tldraw } from 'tldraw'
+import 'tldraw/tldraw.css'
+
+export default function App() {
+	return (
+		<div style={{ position: 'fixed', inset: 0 }}>
+			<Tldraw />
+		</div>
+	)
+}
